edf036cbe35b78009b4359dbaa79ae60ee5c744e
BRENO DE SOUZA ALMEIDA BARROSO <112519@pmenos.com.br>
2023-03-28 18:17:26 -0300

    first commit

---
+++ ASPNET_Core_MVC/Controllers/InfoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace ASPNET_Core_MVC.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Info()
+        {
+            return "versão 1.0.0";
+        }
+        public string NewVersion(string version)
+        {
+            return HtmlEncoder.Default.Encode($"The new version is {version}");
+        }
+    }
+}
